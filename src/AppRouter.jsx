dd0737531df784e458620cb8d33c0ac7b9d2aa97
USER\ASUS <khj50231@g.yju.ac.kr>
2025-05-26 18:52:50 +0900

    오류 수정

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -3,13 +3,13 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import TravelIntro from "./components/Travel/TravelIntro";
 import CreateTravelIntro from "./components/Travel/CreateTravelIntro";
 import DetailTravel from "./components/Travel/DetailTravel";
-import TeamIntro from "./components/Member/TeamIntro";
+// import TeamIntro from "./components/Member/TeamIntro";
 import PostBoard from "./pages/Board.jsx";
 import Login from "./components/Login/Login.jsx";
 import Home from "./Home.jsx";
 import PostDetail from "./pages/PostDetail";
 import Signup from "./components/Login/Signup.jsx"; 
-import MemberDetail from "./components/Member/MemberDetail.jsx";
+
 export default function AppRouter() {
   return (
     <Routes>
@@ -19,10 +19,9 @@ export default function AppRouter() {
       <Route path="/intro" element={<TravelIntro />} />
       <Route path="/intro/new" element={<CreateTravelIntro />} />
       <Route path="/intro/:id" element={<DetailTravel />} />
-      <Route path="/team" element={<TeamIntro />} />
+      {/* <Route path="/team" element={<TeamIntro />} /> */}
       <Route path="/board" element={<PostBoard />} />
       <Route path="/posts/:id" element={<PostDetail />} />
-      <Route path="/posts/:id" element={<MemberDetail />} />
     </Routes>
   );
 }
