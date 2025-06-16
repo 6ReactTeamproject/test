a3f542cef4155942a3e480e52b1d57b01cd57565
USER\ASUS <khj50231@g.yju.ac.kr>
2025-06-16 20:42:09 +0900

    메인화면 슬라이드 구성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../hooks/UserContext";
-
+import TravelCarousel from "../components/Travel&Member/TravelCarousel";
 export default function Home() {
   const navigate = useNavigate();
   const { user: currentUser } = useUser();
@@ -10,6 +10,7 @@ export default function Home() {
       {console.log(currentUser)}
       <div style={{ textAlign: "center", marginTop: "100px" }}>
         <h1>메인 화면</h1>
+        <TravelCarousel />
       </div>
     </>
   );
