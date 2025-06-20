441613f582df9a0ad062d65ccf22d683f2f7e885
뮤냥 <ckdals4693@gmail.com>
2025-06-21 00:51:01 +0900

    component/common 주석

--- src/components/common/FormInput.jsx
+++ src/components/common/FormInput.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// 공용 폼 입력(Input) 필드 컴포넌트
 export default function FormInput({
   name,
   value,
@@ -8,6 +9,7 @@ export default function FormInput({
   type = "text",
   className,
 }) {
+  // HTML 기본 input 태그를 래핑하여 재사용성을 높임
   return (
     <input
       type={type}
--- src/components/common/FormTextarea.jsx
+++ src/components/common/FormTextarea.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// 공용 폼 텍스트 영역(Textarea) 컴포넌트
 export default function FormTextarea({
   name,
   value,
@@ -7,6 +8,7 @@ export default function FormTextarea({
   placeholder,
   className,
 }) {
+  // HTML 기본 textarea 태그를 래핑하여 재사용성을 높임
   return (
     <textarea
       name={name}
